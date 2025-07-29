8cba75a86842bf29f3186939efc4a29b1eed96ec
Pratik Kad <kadpratik22@gmail.com>
2025-07-29 19:03:55 +0530

    sign in signup ui with flash screen

---
+++ SigninSignup/app/src/main/java/com/example/signinsignup/signupActivity.kt
@@ -0,0 +1,35 @@
+package com.example.signinsignup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class signupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+        val signupBtn = findViewById<Button>(R.id.signupBtn)
+        val existBtn = findViewById<TextView>(R.id.existAcc)
+
+        signupBtn.setOnClickListener {
+            Toast.makeText(this, "Signup successful!", Toast.LENGTH_LONG).show()
+        }
+
+        existBtn.setOnClickListener{
+            val intent = Intent(this, loginActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
